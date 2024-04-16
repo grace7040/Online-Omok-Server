a5f6c9d6e31812b8319ea217c20671ebb2ebfd52
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-04-16 23:52:16 +0900

    Init: 프로젝트 생성

---
+++ Server-Project-Com2us/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Server_Project_Com2us.Models;
+using System.Diagnostics;
+
+namespace Server_Project_Com2us.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
