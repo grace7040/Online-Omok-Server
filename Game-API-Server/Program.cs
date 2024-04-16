0ce5aac16f9174b1293407e2d3f8e7bf99d509f1
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-04-17 03:09:00 +0900

    feat: Add Game-API-Server Project

---
+++ Game-API-Server/Program.cs
@@ -0,0 +1,21 @@
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
