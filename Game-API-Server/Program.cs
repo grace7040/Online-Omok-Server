c9b8e45a9c5f517e83ff246be4524131d1cdf829
grace7040 <81251069+grace7040@users.noreply.github.com>
2024-05-21 17:54:51 +0900

    Refactor: GameAPIServer - 컨트롤러에 있던 실질적인 Login/Logout 기능을 서비스에서 수행하도록 수정

--- Game-API-Server/Program.cs
+++ Game-API-Server/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IMemoryDb, RedisDb>();
 builder.Services.AddScoped<IGameDb, MySqlDb>();
 builder.Services.AddSingleton<ICheckAuthService, CheckAuthService>();
+builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<IMatchMakingService, FIFOMatchService>();
 
 
